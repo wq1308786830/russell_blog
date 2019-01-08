77610d32c25d0c17b54fd3a20da81a2a02cda9c1
Russell <flyingcycling@gmail.com>
2019-01-08 11:50:31 +0800

    env

--- russell_react/src/utils/utils.js
+++ russell_react/src/utils/utils.js
@@ -1,32 +1,50 @@
+const env = process.env.NODE_ENV;
+
 export const Config = {
-    // pyPrefix: 'http://104.156.250.95:7001/1.0',
-    // prefix: 'http://104.156.250.95:5001/1.0',
-    prefix: 'http://localhost:5001/1.0',
+  foreignPrefix: 'http://104.156.250.95:7001/1.0',
+  aliyunPrefix: 'http://104.156.250.95:5001/1.0',
+  devPrefix: 'http://localhost:5001/1.0'
 };
 
+console.log(env);
+
 export const options = {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    cache: 'default',
-    body: null
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  cache: 'default',
+  body: null
 };
 
 function checkStatus(response) {
-    if (response.status >= 200 && response.status < 300) {
-        return response;
-    }
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
 
-    const error = new Error(response.statusText);
-    error.response = response;
-    throw error;
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
 }
 
 export default function _fetch(url, options) {
-    return fetch(Config.prefix + url, options)
-        .then(checkStatus)
-        .then(response => response.json())
-        .catch(err => {throw(err)});
+  let prefix = '';
+  switch (env) {
+    case 'development':
+      prefix = Config.devPrefix;
+      break;
+    case 'production':
+      prefix = Config.aliyunPrefix;
+      break;
+    default:
+      break;
+  }
+
+  return fetch(prefix + url, options)
+    .then(checkStatus)
+    .then(response => response.json())
+    .catch(err => {
+      throw err;
+    });
 }

9ed2f1221317eb2f0efbdd47434d9330a6e20215
Russell <flyingcycling@gmail.com>
2019-04-25 17:17:10 +0800

    upgrade libs and react webpack config

--- russell_react/config-overrides.js
+++ russell_react/config-overrides.js
@@ -1,21 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-const lessToJs = require('less-vars-to-js');
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
-const themeVariables = lessToJs(
-  fs.readFileSync(path.join(__dirname, 'src/ant-default-vars.less'), 'utf8')
-);
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
-module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  config = injectBabelPlugin(
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
-    config
-  );
-  config = rewireLess.withLoaderOptions({
-    modifyVars: themeVariables,
-    javascriptEnabled: true
-  })(config, env);
-  return config;
-};
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#5f4b8b' },
+  })
+);

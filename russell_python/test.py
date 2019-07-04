34049202b7d789e23f19feea4947bb919eb36605
Russell <flyingcycling@gmail.com>
2019-07-04 15:48:26 +0800

    fix

---
+++ russell_python/test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'hello world'
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    print(request.headers)
+    print(request.stream.read())
+    return 'welcome'
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
